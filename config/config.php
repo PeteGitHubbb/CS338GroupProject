3f0e06094b59e89a253272b37cd5013475e54c4c
eightrocks <f22fan@uwaterloo.ca>
2024-07-21 11:29:58 -0400

    d

--- config/config.php
+++ config/config.php
@@ -1,9 +1,9 @@
 <?php
 $config = [
 'servername' => '127.0.0.1',
-'username' => 'stagqw12',
-'password' => 'YuriaInvanova1245',
-'dbname' => 'ttctransitinfosystem'
+'username' => 'IGP',
+'password' => 'choochoo123!!6',
+'dbname' => 'ttccompany'
 ];
 ?>
 
