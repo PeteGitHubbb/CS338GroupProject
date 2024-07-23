78f627931306c5259a10cb4524fa0ccc504c451a
Edward Zhang <104185767+edward-zha@users.noreply.github.com>
2024-07-23 12:22:26 -0400

    Update and rename config.php to config_sample.php

--- config/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-$config = [
-'servername' => '127.0.0.1',
-'username' => 'IGP',
-'password' => 'choochoo123!!6',
-'dbname' => 'ttccompany'
-];
-?>
-
---
+++ config/config_sample.php
@@ -0,0 +1,9 @@
+<?php
+$config = [
+'servername' => '127.0.0.1',
+'username' => 'sample_username',
+'password' => 'sample_password',
+'dbname' => 'sample_db'
+];
+?>
+
